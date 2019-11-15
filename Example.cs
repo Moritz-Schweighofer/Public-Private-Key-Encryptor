9748c652c71456b8eb3b3d6a65cc8e8c5ea873f0
Moritz-Schweighofer <56548437+Moritz-Schweighofer@users.noreply.github.com>
2019-11-15 11:10:07 +0100

    Fist Commit
    Usable but limitied Functionality

---
+++ Example.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Public_Private_Key_Encryptor
+{
+    class Example
+    {
+        static void Main(string[] args)
+        {
+            string private_key = "<RSAKeyValue><Modulus>VsABn18QLT9ee3x5Mk8RCqNIaFFUu1watVI0HLz5kF0vi0jomGWXTLX55tDfSJTHqMTA339R+9XcOSTnXHX1NQ==</Modulus><Exponent>AQAB</Exponent><P>l86VkmzzW+BjWx5LPZ45a/MdLraZnRgSpIpJf7LHnyE=</P><Q>kkqEjdWnKUGIieJOJoEXDPgdu7NFn6elUcEmcl+Od5U=</Q><DP>czvp5iC2CsQmJ4CzdK6qv/rn6BHMQaeIX0ZpzHmVdoE=</DP><DQ>aytyHiH1+vBKYZDLZcPOKi8eQSKdD9AV+WWBj+pXCV0=</DQ><InverseQ>QyTluXg8PtDmo1BbkpPPX0a2WpRhUbHimHUMQrMfUfE=</InverseQ><D>Ozeso98fytNkKsWIcgg5KNrXdaZ3QcKY1Me9nRR/A8VdTlmTFcWerCjwZUrgPwZK/eWWyQqzFCuQpliNzMC+gQ==</D></RSAKeyValue>";
+            string public_key = "<RSAKeyValue><Modulus>VsABn18QLT9ee3x5Mk8RCqNIaFFUu1watVI0HLz5kF0vi0jomGWXTLX55tDfSJTHqMTA339R+9XcOSTnXHX1NQ==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
+
+
+            RSAController rsa = new RSAController(private_key, public_key); // The RSAController needs the private and public Key
+
+            Console.WriteLine("Enter The Password: ");
+
+            string Password = Console.ReadLine();
+
+            string Password_Encrypded = rsa.Encrypt(Password); //Encrypt the Password
+
+            string m_exePath = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            try
+            {
+                using (StreamWriter w = File.AppendText(m_exePath + "\\Password_Encrypted.txt"))
+                {
+                    w.WriteLine(Password_Encrypded);
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+
+        }
+    }
+}
---
+++ RSAController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Public_Private_Key_Encryptor
+{
+    internal class RSAController
+    {
+        private string _private_key;
+        private string _public_key;
+
+        private static UnicodeEncoding _encoder = new UnicodeEncoding();
+
+        public RSAController(string private_key, string public_key)
+        {
+            _public_key = public_key;
+            _private_key = private_key;
+        }
+
+        public string Encrypt(string data)
+        {
+            var rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(_public_key);
+            var dataToEncrypt = _encoder.GetBytes(data);
+            var encryptedByteArray = rsa.Encrypt(dataToEncrypt, false).ToArray();
+            var length = encryptedByteArray.Count();
+            var item = 0;
+            var sb = new StringBuilder();
+            foreach (var x in encryptedByteArray)
+            {
+                item++;
+                sb.Append(x);
+
+                if (item < length)
+                    sb.Append(",");
+            }
+
+            return sb.ToString();
+        }
+
+        public string Decrypt(string data)
+        {
+            var rsa = new RSACryptoServiceProvider();
+            var dataArray = data.Split(new char[] { ',' });
+            byte[] dataByte = new byte[dataArray.Length];
+            for (int i = 0; i < dataArray.Length; i++)
+            {
+                dataByte[i] = Convert.ToByte(dataArray[i]);
+            }
+
+            rsa.FromXmlString(_private_key);
+            var decryptedByte = rsa.Decrypt(dataByte, false);
+            return _encoder.GetString(decryptedByte);
+        }
+    }
+}
