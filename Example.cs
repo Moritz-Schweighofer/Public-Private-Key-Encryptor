cce33b1f4e34befd039c87a019cae99694155807
Moritz Schweighofer <moritz.schweighofer@dmgmori.com>
2019-11-17 11:02:08 +0100

    Class Updated
    -No more Private key needed for generating a class object
    - general fixes

--- Example.cs
+++ Example.cs
@@ -4,61 +4,57 @@ using System.Reflection;
 
 namespace Public_Private_Key_Encryptor
 {
-    class Example
+    internal class Example
     {
-
         /* Note:
-         * 
+         *
          * This Example Takes the User Input,
          * Encrypts it and saves the Encrypted Message
          * into the Assembly Folder
-         * 
+         *
          * It also writes the Decrypted Password
          * in another file
-         * 
+         *
          */
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            string private_key = "<RSAKeyValue><Modulus>VsABn18QLT9ee3x5Mk8RCqNIaFFUu1watVI0HLz5kF0vi0jomGWXTLX55tDfSJTHqMTA339R+9XcOSTnXHX1NQ==</Modulus><Exponent>AQAB</Exponent><P>l86VkmzzW+BjWx5LPZ45a/MdLraZnRgSpIpJf7LHnyE=</P><Q>kkqEjdWnKUGIieJOJoEXDPgdu7NFn6elUcEmcl+Od5U=</Q><DP>czvp5iC2CsQmJ4CzdK6qv/rn6BHMQaeIX0ZpzHmVdoE=</DP><DQ>aytyHiH1+vBKYZDLZcPOKi8eQSKdD9AV+WWBj+pXCV0=</DQ><InverseQ>QyTluXg8PtDmo1BbkpPPX0a2WpRhUbHimHUMQrMfUfE=</InverseQ><D>Ozeso98fytNkKsWIcgg5KNrXdaZ3QcKY1Me9nRR/A8VdTlmTFcWerCjwZUrgPwZK/eWWyQqzFCuQpliNzMC+gQ==</D></RSAKeyValue>";
+            string private_key = "<RSAKeyValue><Modulus>VsABn18QLT9ee3x5Mk8RCqNIaFFUu1watVI0HLz5kF0vi0jomGWXTLX55tDfSJTHqMTA339R+9XcOSTnXHX1NQ==</Modulus><Exponent>AQAB</Exponent>" +
+                "<P>l86VkmzzW+BjWx5LPZ45a/MdLraZnRgSpIpJf7LHnyE=</P><Q>kkqEjdWnKUGIieJOJoEXDPgdu7NFn6elUcEmcl+Od5U=</Q><DP>czvp5iC2CsQmJ4CzdK6qv/rn6BHMQaeIX0ZpzHmVdoE=</DP>" +
+                "<DQ>aytyHiH1+vBKYZDLZcPOKi8eQSKdD9AV+WWBj+pXCV0=</DQ><InverseQ>QyTluXg8PtDmo1BbkpPPX0a2WpRhUbHimHUMQrMfUfE=</InverseQ><D>Ozeso98fytNkKsWIcgg5KNrXdaZ3QcKY1Me9nRR/A8VdTlmTFcWerCjwZUrgPwZK/eWWyQqzFCuQpliNzMC+gQ==</D></RSAKeyValue>";
+
             string public_key = "<RSAKeyValue><Modulus>VsABn18QLT9ee3x5Mk8RCqNIaFFUu1watVI0HLz5kF0vi0jomGWXTLX55tDfSJTHqMTA339R+9XcOSTnXHX1NQ==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
 
-
-            RSAController rsa = new RSAController(private_key, public_key); // The RSAController needs the private and public Key
+            RSAController rsa = new RSAController(); // Generate the RSA Controller
 
             Console.WriteLine("Enter The Password: ");
 
             string Password = Console.ReadLine();
 
-            string Password_Encrypded = rsa.Encrypt(Password); //Encrypt the Password
+            string Password_Encrypded = rsa.Encrypt(Password, public_key); //Encrypt the Password
+
+            string Password_Decrypted = rsa.Decrypt(Password_Encrypded, private_key); //Decrypt the Password
+
 
             string m_exePath = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
             try
             {
-                using (StreamWriter w = File.AppendText(m_exePath + "\\Password_Encrypted.txt"))
-                {
-                    w.WriteLine(Password_Encrypded);
-                }
+                using StreamWriter w = File.AppendText(m_exePath + "\\Password_Encrypted.txt");
+                w.WriteLine(Password_Encrypded);
             }
             catch (Exception)
             {
             }
 
-            string Password_Decrypted = rsa.Decrypt(Password_Encrypded); //Decrypt the Password
-
             try
             {
-                using (StreamWriter w = File.AppendText(m_exePath + "\\Password_Decrypted.txt"))
-                {
-                    w.WriteLine(Password_Decrypted);
-                }
+                using StreamWriter w = File.AppendText(m_exePath + "\\Password_Decrypted.txt");
+                w.WriteLine(Password_Decrypted);
             }
             catch (Exception)
             {
             }
-
-
-
         }
     }
 }
--- RSAController.cs
+++ RSAController.cs
@@ -7,51 +7,52 @@ namespace Public_Private_Key_Encryptor
 {
     internal class RSAController
     {
-        private string _private_key;
-        private string _public_key;
 
         private static UnicodeEncoding _encoder = new UnicodeEncoding();
 
-        public RSAController(string private_key, string public_key)
+        public RSAController()
         {
-            _public_key = public_key;
-            _private_key = private_key;
+            //Pass
         }
 
-        public string Encrypt(string data)
+        public string Encrypt(string data, string _public_key)
         {
-            var rsa = new RSACryptoServiceProvider();
-            rsa.FromXmlString(_public_key);
-            var dataToEncrypt = _encoder.GetBytes(data);
-            var encryptedByteArray = rsa.Encrypt(dataToEncrypt, false).ToArray();
-            var length = encryptedByteArray.Count();
-            var item = 0;
-            var sb = new StringBuilder();
-            foreach (var x in encryptedByteArray)
+            try
             {
-                item++;
-                sb.Append(x);
+                var rsa = new RSACryptoServiceProvider();
+                rsa.FromXmlString(_public_key);
+                var dataToEncrypt = _encoder.GetBytes(data);
 
-                if (item < length)
-                    sb.Append(",");
+                var encryptedByteArray = rsa.Encrypt(dataToEncrypt, false);
+                var encryptedString = Convert.ToBase64String(encryptedByteArray);
+
+                return encryptedString;
             }
-
-            return sb.ToString();
+            catch (CryptographicException ex)
+            {
+                Console.WriteLine("Error With Encrypting the Password: " + ex.Message);
+                return "";
+            }
         }
 
-        public string Decrypt(string data)
+        public string Decrypt(string data, string _private_key)
         {
-            var rsa = new RSACryptoServiceProvider();
-            var dataArray = data.Split(new char[] { ',' });
-            byte[] dataByte = new byte[dataArray.Length];
-            for (int i = 0; i < dataArray.Length; i++)
-            {
-                dataByte[i] = Convert.ToByte(dataArray[i]);
-            }
 
-            rsa.FromXmlString(_private_key);
-            var decryptedByte = rsa.Decrypt(dataByte, false);
-            return _encoder.GetString(decryptedByte);
+            try
+            {
+                var rsa = new RSACryptoServiceProvider();
+                var dataByte = Convert.FromBase64String(data);
+
+                rsa.FromXmlString(_private_key);
+                var decryptedByte = rsa.Decrypt(dataByte, false);
+
+                return _encoder.GetString(decryptedByte);
+            }
+            catch (CryptographicException ex)
+            {
+                Console.WriteLine("Error With Decrypting the Password: " + ex.Message);
+                return "";
+            }
         }
     }
 }
